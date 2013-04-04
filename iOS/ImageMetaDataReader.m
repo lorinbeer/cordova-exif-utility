74973ca8a3ae739c2bbb48651dd3f5e1d24897af
lorinbeer <lorin@adobe.com>
2013-04-04 09:25:19 -0700

    Updated image meta data reader to properly handle iOS update CGImageSourceRef dictionary meta data

--- iOS/ImageMetaDataReader.m
+++ iOS/ImageMetaDataReader.m
@@ -1,4 +1,4 @@
-#import "ExifReader.h"
+#import "ImageMetaDataReader.h"
 #import <ImageIO/ImageIO.h>
 
 @implementation ExifReader
@@ -6,23 +6,25 @@
 - (void) getExifData:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
     NSURL * url = [NSURL URLWithString: [command.arguments objectAtIndex:0]];
-    NSString * metatarget = [NSString stringWithUTF8String:[command.arguments objectAtIndex:1]];
     
     if (url && url.scheme && url.host) {
         
         // avoids loading the entire img
-        CGImageSourceRef imgref = CGImageSourceCreateWithURL((CFURLRef)url,NULL);
-
-        // right now, we are only interested in the Exif data, so we pull it out of the meta data dict returned by the following mouthful
-        NSDictionary * imgMeta = [(NSDictionary*)CGImageSourceCopyPropertiesAtIndex(imgref, 0, NULL) objectForKey:metatarget];
+        CGImageSourceRef imgref         = CGImageSourceCreateWithURL((CFURLRef)url,NULL);
         
+        // right now, we are only interested in the Exif data, so we pull it out of the meta data dict returned by the following mouthful
+        CFDictionaryRef ref = CGImageSourceCopyPropertiesAtIndex(imgref, 0, NULL);
+        CFDictionaryRef exif = CFDictionaryGetValue(ref,kCGImagePropertyExifDictionary);
+        // NSDictionary * imgMeta = [(NSDictionary*)CGImageSourceCopyPropertiesAtIndex(imgref, 0, NULL) objectForKey:@"{Exif}"];
+        NSDictionary * imgMeta = (NSDictionary*)exif;
+        NSLog(@"%@", [(NSDictionary*)ref description]);
         if (imgMeta) {
             // just in case
             NSError * serialerror = nil;
-        
+            
             // attempt to serialize the meta data dict
             NSData * dat = [NSJSONSerialization dataWithJSONObject: imgMeta options: 0 error: &serialerror];
-                
+            
             if (serialerror == nil) {
                 NSString * jsonstr = [[NSString alloc]initWithData:dat encoding:NSUTF8StringEncoding];
                 pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: jsonstr];
@@ -41,13 +43,6 @@
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (NSDictionary*) getMiscMetaDataFromInfoDict: (NSDictionary*) info {
-    return [NSDictionary dictionaryWithObjectsAndKeys:
-                [info objectForKey:@"DPIHeight"], @"DPIHeight",
-                [info objectForKey:@"DPIWidth"], @"DPIWidth",
-                [info objectForKey:@"Orientation"], @"Orientation", nil];
 }
 
 @end
