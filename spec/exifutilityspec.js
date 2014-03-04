3e360daa11c5c1497a8812884b38ebaaa7a0dc47
lorinbeer <lorin@adobe.com>
2014-03-04 14:42:10 -0800

    tracking js unit tests

---
+++ spec/exifutilityspec.js
@@ -0,0 +1,25 @@
+
+
+describe("Exif Reader", function () {
+
+    beforeEach(function () {});
+
+
+    it("should exist", function() {
+        expect(ExifReader).toBeDefined();
+    });
+  
+    it("should have an x function", function() {
+
+    });
+
+    it("should have a y function", function() {});
+
+    it("", function() {});
+
+    it("", function() {});
+
+    it("", function() {});
+
+
+});
