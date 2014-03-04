cb5b5308db87bceb465227963eba67e92d33642a
lorinbeer <lorin@adobe.com>
2014-03-04 15:25:38 -0800

    skeleton of js testing spec

--- spec/exifutilityspec.js
+++ spec/exifutilityspec.js
@@ -1,25 +1,105 @@
 
 
 describe("Exif Reader", function () {
+    var cordova;
 
-    beforeEach(function () {});
-
+    beforeEach(function () {
+       cordova = jasmine.createSpyObj('cordova', ['','']);
+    });
 
     it("should exist", function() {
         expect(ExifReader).toBeDefined();
     });
   
-    it("should have an x function", function() {
+    describe("getExifData function", function () {
+        beforeEach(function () {
+        
+        });
+
+        it("should exist", function() {
+            expect(ExifReader.getExifData).toBeDefined();
+        });
+
+        it("should have been called with a path and pass/fail callbacks", function () {
+
+        });
+
+        it("should call success callback with a valid js object containing keyed data", function () {
+
+        });
+
+        it("should call error callback with an error message", function () {
+
+        });
 
     });
 
-    it("should have a y function", function() {});
 
-    it("", function() {});
+    describe("getTiffData function", function () {
+        beforeEach(function () {
 
-    it("", function() {});
+        });
 
-    it("", function() {});
+        it("should exist", function() {
+            expect(ExifReader.getExifData).toBeDefined();
+        });
 
+        it("should have been called with a path and pass/fail callbacks", function () {
+
+        });
+
+        it("should return a valid js object", function () {
+
+        });
+
+    });
+
+
+    describe("stripExifData function", function () {
+        beforeEach(function () {
+
+        });
+
+        it("should exist", function() {
+            expect(ExifReader.getExifData).toBeDefined();
+        });
+
+        it("should have been called with a path and pass/fail callbacks", function () {
+
+        });
+
+        it("should call success callback with path to modified image", function () {
+
+        });
+
+        it("should call error callback with error message", function () {
+
+        });
+
+    });
+
+
+    describe("stripGeoData function", function () {
+        beforeEach(function () {
+
+        });
+
+        it("should exist", function() {
+            expect(ExifReader.getExifData).toBeDefined();
+        });
+
+        it("should have been called with a path and pass/fail callbacks", function () {
+
+        });
+
+        it("should call success callback with path to modified image ", function () {
+
+        });
+
+        it("should call error callback with error message", function () {
+
+        });
+
+    });
 
 });
